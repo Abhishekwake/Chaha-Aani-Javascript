1f038d9c6a41bf479d8f9011b3d0b5c994982734
Abhishek Wake <abhishekwake111@gmail.com>
2025-04-05 12:48:47 +0530

    Learn Javascipt varibles

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144556;
+let accountEmail = "abc@google.com";
+var accountPass = "1234";
+accountCity = "wardha";
+let accountState;
+
+// accountId = 232323; //not allowed to change the constant
+
+accountEmail = "hk@faceboom.com";
+accountPass = "4444";
+accountState = "Jaipur";
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPass, accountState]);
